e1d30888d643298a050979b1c2dff01238fa0e9a
William Jackson <108308676+WillJackson2022@users.noreply.github.com>
2022-09-12 23:22:21 -0400

    Update wk13_name_gen_EC2.py
    Updated code

--- wk13_name_gen_EC2.py
+++ wk13_name_gen_EC2.py
@@ -12,8 +12,8 @@ ec2_instances = int(input("How many instances will you need to run? "))
 dept = str(input("What department are you in? "))
 
 #Random characters and numbers that will be generated and included in the unique name for EC2
-char_numb = ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.digits) for _ in range(10))
-print(char_numb)
+random_name = ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.digits) for _ in range(10))
+print(random_name)
 
 #Selected department and EC2 name
-print((dept + '_' + char_numb) + ' ' + 'is your department EC2 name')
+print((dept + '_' + random_name) + ' ' + 'is your department EC2 name')
